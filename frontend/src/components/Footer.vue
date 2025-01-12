4c7d4c9f671a9eb69e2131684f2a03138774dd00
DODANGHOAN <dodanghoana12017@gmail.com>
2025-01-13 02:57:04 +0700

    html_css_basic_Home&HomePopup

---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,27 @@
+<!-- src/components/AppFooter.vue -->
+<template>
+<footer>
+    <p>&copy; 2025 Trang web bán sách. All rights reserved.</p>
+    <!-- Thêm các liên kết hoặc thông tin khác tại đây -->
+</footer>
+</template>
+
+<script>
+export default {
+name: 'AppFooter'
+}
+</script>
+
+<style scoped>
+footer {
+background-color: #f1f1f1;
+padding: 20px;
+text-align: center;
+position: fixed;
+bottom: 0;
+left: 0;
+width: 100%;
+margin: 0;
+}
+</style>
+  
